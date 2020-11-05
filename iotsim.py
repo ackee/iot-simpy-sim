9a83e7a8c89048290434e8b9e4a0910cfe69eb4b
Alexander Norberg <ackess@gmail.com>
2020-11-05 19:28:16 +0100

    Changed the wording to better fit what it is, a fog(cloud/edge) network.

--- iotsim.py
+++ iotsim.py
@@ -16,7 +16,7 @@ import matplotlib.pyplot as plt
 # All times unless otherwise specified are in seconds
 
 RANDOM_SEED = 42           # Not so random but we want it reproducible
-NUM_MACHINES = 1           # Number of calc machines.
+NUM_MACHINES = 1           # Number of available machines in the cloud.
 NUM_SERVERS = 2            # Number of gateway nodes that are connected to sensors
 NUM_DEVICES = 10           # Number of edge devices
 PROCESS_TIME = 1000        # Time it takes to calculate 
@@ -26,9 +26,9 @@ SIM_TIME = 4000000         # Simulation time in seconds
 
 time_data = []          # List of dictionaries to store the data of how long a request takes.
 
-class Datacenter(object):
+class Cloud(object):
     """
-    A datacenter with limited number of rented machines A.K.A The cloud.
+    A cloud with limited number of rented machines A.K.A The cloud.
 
     Gateway nodes can request one of the machines (1 default). When they get one
     the calculation can start. It takes processtime seconds.
@@ -45,15 +45,15 @@ class Datacenter(object):
         """
         yield self.env.timeout(self.processtime)
 
-class Serverfarm(object):
+class Gateway(object):
     """
     Our gateway devices, hopefully only one is needed.
     """
-    def __init__(self, env, num_servers, processtime, datacenter):
+    def __init__(self, env, num_servers, processtime, cloud):
         self.env = env
         self.server = simpy.PriorityResource(env, num_servers)
         self.processtime = processtime
-        self.datacenter = datacenter
+        self.cloud = cloud
 
     def proc(self, val):
         """
@@ -63,13 +63,13 @@ class Serverfarm(object):
         #Roughly 30% of all requests are heavy calculations.
         if random.randint(0,1) < 0.3:
             print("Process is heavy sending to data center!")
-            yield env.process(self.datacenter.calc(val))
+            yield env.process(self.cloud.calc(val))
         else:
             print("Process light, handle at server.")
             yield self.env.timeout(self.processtime)
 
 
-def device(env, name, serverfarm):
+def device(env, name, gateway):
     """The device process (each device has a ``name``) sends a request for data
     from (``server``).
 
@@ -78,11 +78,11 @@ def device(env, name, serverfarm):
     """
     
     starttime = env.now
-    with serverfarm.server.request() as request:
+    with gateway.server.request() as request:
         yield request
 
         getsprocessed = env.now
-        yield env.process(serverfarm.proc(name))
+        yield env.process(gateway.proc(name))
 
         getsanswered = env.now
         wt = getsprocessed - starttime
@@ -91,23 +91,23 @@ def device(env, name, serverfarm):
         time_data.append({"waitTime": wt/1000, "processTime": pt/1000, "totalTime": tt/1000})
 
 def setup(env, num_devices, num_machines, num_servers, processtime, computetime, t_inter):
-    """Create a datacenter, a number of servers, a number of initial device requests and keep creating cars
+    """Create a cloud, a number of servers, a number of initial device requests and keep creating cars
     approx. every ``t_inter`` minutes."""
-    # Create the datacenter
-    datacenter = Datacenter(env, num_machines, computetime)
+    # Create the Cloud object
+    cloud = Cloud(env, num_machines, computetime)
 
-    # Create the serverfarm
-    serverfarm = Serverfarm(env, num_servers, processtime, datacenter)
+    # Create the Gateway system
+    gateway = Gateway(env, num_servers, processtime, cloud)
 
     # Create 4 initial iot data requests
     for i in range(4):
-        env.process(device(env, 'IOT %d' % i, serverfarm))
+        env.process(device(env, 'IOT %d' % i, gateway))
 
     # Create more iot data requests while the simulation is running
     while True:
         yield env.timeout(t_inter/num_devices)
         i += 1
-        env.process(device(env, 'IOT %d' % i, serverfarm))
+        env.process(device(env, 'IOT %d' % i, gateway))
 
 # Setup and start the simulation
 print('IOT Network simulation')
